b722d39f2be2252b981dc95adadab7a7a06117b5
Steve <steve.richard3592@gmail.com>
2025-01-17 12:04:33 -0800

    finished ui design

--- Front_End/src/components/messages/messageContainer.tsx
+++ Front_End/src/components/messages/messageContainer.tsx
@@ -1,18 +1,41 @@
 import Messages from "./messages.tsx"
+import SendMessage from "./sendMessage.tsx"
+import { TiMessage } from "react-icons/ti";
 
 const messageContainer = () => {
+  const chatNotSelected = true;
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-        <>
+      {chatNotSelected ? (
+        noChatSelected()
+      ) :(
+        <> 
             {/* Header */}
-            <div className="bg-slate-600 px-4 py-2 mb-2">
+            <div className="bg-blue-700 px-4 py-2 mb-2">
                 <span className="label-text">To: </span>{" "}
-                <span className="text-gray-800 font-bold">John Doe</span>
+                <span className="text-white font-bold">John Doe</span>
             </div>
             <Messages />
+            <SendMessage />
         </>
+      )
+    }
     </div>
   )
 }
 
 export default messageContainer
+
+const noChatSelected = () =>{
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-600 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessage />
+      </div>
+
+    </div>
+  );
+};
